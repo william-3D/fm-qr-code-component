9208afdf477197cbec89bfc2316271afd86a80f8
Sonia William <sonia@artisan.com.my>
2023-12-07 14:59:39 +0800

    add qr-code and card to page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="page-container">
-    <div class="tw-text-red-500">Hello</div>
+    <div class="card">
+      <q-img
+      class="qr-code" 
+      src="/assets/images/image-qr-code.png"
+      />
+    </div>
   </div>
 </template>
 
