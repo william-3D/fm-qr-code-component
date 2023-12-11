9a48ddbb08071d728084a565932a273b3939dab0
soniaawilliam <sonia@artisan.com.my>
2023-12-11 15:30:54 +0800

    add text

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
       class="qr-code" 
       src="/assets/images/image-qr-code.png"
       />
+      <div class="title">Improve your front-end skills by building projects</div>
+      <div class="subtitle">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
     </div>
   </div>
 </template>
