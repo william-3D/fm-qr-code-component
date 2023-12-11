329fe5070513a455ed5c68d2802a563784af3bd3
soniaawilliam <sonia@artisan.com.my>
2023-12-11 16:12:30 +0800

    style for mobile view

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="page-container">
     <div class="card">
-      <q-img
-      class="qr-code" 
-      src="/assets/images/image-qr-code.png"
-      />
-      <div class="title">Improve your front-end skills by building projects</div>
-      <div class="subtitle">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+      <div class="card-content-wrapper">
+        <q-img
+        class="qr-code"
+        src="/assets/images/image-qr-code.png"
+        />
+        <div class="title">Improve your front-end skills by building projects</div>
+        <div class="subtitle">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+      </div>
     </div>
   </div>
 </template>
